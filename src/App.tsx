054e96c7fa12d63993e1ebf4fcd94fa7e3c31653
Hi guys, my name is NGUYEN VAN DUC <98450746+Vanduc006@users.noreply.github.com>
2024-11-12 14:40:23 +0000

    add tutor page

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@
 import { useRef, useEffect, useState} from 'react';
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle, CardFooter, CardDescription } from "@/components/ui/card"
-import { LayoutDashboard, Home, Info,Plus, Menu, Flame,Trash2, UserCog } from "lucide-react"
+import { LayoutDashboard, Home, Info,Plus, Menu, Flame,Trash2, UserCog, GraduationCap } from "lucide-react"
 import { SignedIn, SignedOut, SignIn, UserButton, useUser } from '@clerk/clerk-react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import {
@@ -15,15 +15,6 @@ import {
   TableRow,
 } from "@/components/ui/table"
 
-// import {
-//   Dialog,
-//   DialogContent,
-//   DialogDescription,
-//   DialogFooter,
-//   DialogHeader,
-//   DialogTitle,
-//   DialogTrigger,
-// } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Badge } from "./components/ui/badge"
@@ -55,15 +46,12 @@ import 'react-toastify/dist/ReactToastify.css';
 import Datenow from './sections/Datenow';
 import { createClient } from "@supabase/supabase-js";
 // import supabase from './controllers/ConnectSupaBase';
-
-
-
 // import { BartChartPowerBi } from "./sections/BartChartPowerBi"
 // import AreaChartPowerBi from "./sections/AreaChartPowerBi"
 // import { PieChartPowerBi } from "./sections/PieChartPowerBi"
 // import { currentUser } from '@clerk/nextjs/server';
 import Marquee from "react-fast-marquee";
-
+import Tutor from "./sections/Tutor"
 
 const currentDate = new Date();
 const formattedDate = currentDate.toLocaleDateString('vi-VN', {
@@ -72,65 +60,63 @@ const formattedDate = currentDate.toLocaleDateString('vi-VN', {
   day: '2-digit',
 });
 
-// const supabase = createClient('https://myfdqawmzovlouhecepo.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im15ZmRxYXdtem92bG91aGVjZXBvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTcyMjA4NTUsImV4cCI6MjAxMjc5Njg1NX0.8UXB-5owTd7tudPSVR80j7pQCSuNON342wGVWvXZXZU');
-// const { data } = await supabase.from('powerbi').select('*')
-// console.log(data)
-// const articles = data;
-// const tableData = data;
+const supabase = createClient('https://myfdqawmzovlouhecepo.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im15ZmRxYXdtem92bG91aGVjZXBvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTcyMjA4NTUsImV4cCI6MjAxMjc5Njg1NX0.8UXB-5owTd7tudPSVR80j7pQCSuNON342wGVWvXZXZU');
+const { data } = await supabase.from('powerbi').select('*')
+console.log(data)
+const articles = data;
+const tableData = data;
 
 const admins = [
-  'c3435101@uon.edu.au',
+  // 'c3435101@uon.edu.au',
   'janie.nguyen2059@gmail.com',
   'quynhanh.ng520@gmail.com',
-  'bagiadaohoa@gmail.com',
-  'ducnv2411150@usth.edu.vn'
 ]
-const articles = [
-  {
-    id: 1,
-    title: "The Future of AI",
-    image: "https://cdn.tgdd.vn/News/1561019/2(27)-1280x720.jpg",
-    content: "Artificial Intelligence is rapidly evolving, transforming industries and our daily lives. From self-driving cars to advanced medical diagnostics, AI is pushing the boundaries of what's possible.",
-    author: "Jane Doe",
-    modify: "2024-03-15",
-    powerBiLink: "https://app.powerbi.com/reportEmbed?reportId=3be9ae9d-b37d-4b8b-becd-bbdc51b78348&autoAuth=true&embeddedDemo=true"
-  },
-  {
-    id: 2,
-    title: "Sustainable Energy Solutions",
-    image: "https://cdn.tgdd.vn/News/1561019/2(27)-1280x720.jpg",
-    content: "As the world grapples with climate change, sustainable energy solutions are becoming increasingly crucial. Solar, wind, and other renewable sources are paving the way for a greener future.",
-    author: "John Smith",
-    modify: "2024-03-14",
-    powerBiLink: "https://app.powerbi.com/view?r=eyJrIjoiOGZhYjNkNTktZGE5Zi00MWE1LWEyNzktNmRhODJiOGM5YmJiIiwidCI6ImRmODY3OWNkLWE4MGUtNDVkOC05OWFjLWM4M2VkN2ZmOTVhMCJ9"
-  },
-  {
-    id: 3,
-    title: "The Rise of Remote Work",
-    image: "https://cdn.tgdd.vn/News/1561019/2(27)-1280x720.jpg",
-    content: "The global pandemic has accelerated the adoption of remote work, leading to a paradigm shift in how we view the workplace. Companies are reimagining their policies to adapt to this new reality.",
-    author: "Alice Johnson",
-    modify: "2024-03-13",
-    powerBiLink: "https://app.powerbi.com/view?r=eyJrIjoiYTZjZjBiNTktYTJiZC00MjY4LWExMDUtNGY5NWJmZGUzOWQ2IiwidCI6ImRmODY3OWNkLWE4MGUtNDVkOC05OWFjLWM4M2VkN2ZmOTVhMCJ9"
-  },
-  {
-    id: 4,
-    title: "Cybersecurity in the Digital Age",
-    image: "https://cdn.tgdd.vn/News/1561019/2(27)-1280x720.jpg",
-    content: "As our lives become increasingly digital, the importance of cybersecurity cannot be overstated. From personal data protection to national security, the challenges and solutions are evolving rapidly.",
-    author: "Bob Williams",
-    modify: "2024-03-12",
-    powerBiLink: "https://app.powerbi.com/view?r=eyJrIjoiNzhhMTg2ZTgtZTMzNC00MmMwLWE1YTktNDgyZjU3OWY5NzkwIiwidCI6ImRmODY3OWNkLWE4MGUtNDVkOC05OWFjLWM4M2VkN2ZmOTVhMCJ9"
-  }
-]
-const tableData = articles;
+// const articles = [
+//   {
+//     id: 1,
+//     title: "The Future of AI",
+//     image: "https://cdn.tgdd.vn/News/1561019/2(27)-1280x720.jpg",
+//     content: "Artificial Intelligence is rapidly evolving, transforming industries and our daily lives. From self-driving cars to advanced medical diagnostics, AI is pushing the boundaries of what's possible.",
+//     author: "Jane Doe",
+//     modify: "2024-03-15",
+//     powerBiLink: "https://app.powerbi.com/reportEmbed?reportId=3be9ae9d-b37d-4b8b-becd-bbdc51b78348&autoAuth=true&embeddedDemo=true"
+//   },
+//   {
+//     id: 2,
+//     title: "Sustainable Energy Solutions",
+//     image: "https://cdn.tgdd.vn/News/1561019/2(27)-1280x720.jpg",
+//     content: "As the world grapples with climate change, sustainable energy solutions are becoming increasingly crucial. Solar, wind, and other renewable sources are paving the way for a greener future.",
+//     author: "John Smith",
+//     modify: "2024-03-14",
+//     powerBiLink: "https://app.powerbi.com/view?r=eyJrIjoiOGZhYjNkNTktZGE5Zi00MWE1LWEyNzktNmRhODJiOGM5YmJiIiwidCI6ImRmODY3OWNkLWE4MGUtNDVkOC05OWFjLWM4M2VkN2ZmOTVhMCJ9"
+//   },
+//   {
+//     id: 3,
+//     title: "The Rise of Remote Work",
+//     image: "https://cdn.tgdd.vn/News/1561019/2(27)-1280x720.jpg",
+//     content: "The global pandemic has accelerated the adoption of remote work, leading to a paradigm shift in how we view the workplace. Companies are reimagining their policies to adapt to this new reality.",
+//     author: "Alice Johnson",
+//     modify: "2024-03-13",
+//     powerBiLink: "https://app.powerbi.com/view?r=eyJrIjoiYTZjZjBiNTktYTJiZC00MjY4LWExMDUtNGY5NWJmZGUzOWQ2IiwidCI6ImRmODY3OWNkLWE4MGUtNDVkOC05OWFjLWM4M2VkN2ZmOTVhMCJ9"
+//   },
+//   {
+//     id: 4,
+//     title: "Cybersecurity in the Digital Age",
+//     image: "https://cdn.tgdd.vn/News/1561019/2(27)-1280x720.jpg",
+//     content: "As our lives become increasingly digital, the importance of cybersecurity cannot be overstated. From personal data protection to national security, the challenges and solutions are evolving rapidly.",
+//     author: "Bob Williams",
+//     modify: "2024-03-12",
+//     powerBiLink: "https://app.powerbi.com/view?r=eyJrIjoiNzhhMTg2ZTgtZTMzNC00MmMwLWE1YTktNDgyZjU3OWY5NzkwIiwidCI6ImRmODY3OWNkLWE4MGUtNDVkOC05OWFjLWM4M2VkN2ZmOTVhMCJ9"
+//   }
+// ]
+// const tableData = articles;
 export default function App() {
   const [rows, setRows] = useState(tableData);
   const [activePage, setActivePage] = useState("Dashboard")
   const [sidebarOpen, setSidebarOpen] = useState(false)
   // const [selectedRows, setSelectedRows] = useState<string[]>([])
   const [selectedArticle, setSelectedArticle] = useState<typeof articles[0] | null>(null)
-  const pages = ["Dashboard"]
+  const pages = ["Dashboard","Tutorial"]
   
   const openDrawer = (article: typeof articles[0]) => {
     setSelectedArticle(article)
@@ -149,7 +135,6 @@ export default function App() {
                 
           <SignedOut>
           
-            {/* Hiển thị nếu người dùng chưa đăng nhập */}
             <SignIn/>
           </SignedOut>
         </div>
@@ -162,14 +147,13 @@ export default function App() {
   } 
   
   const handleDelete = async (id) => {
-    // Here you would execute the delete operation, like calling the API or supabase delete function
+
     await supabase
       .from('powerbi')
       .delete()
       .eq('id', id);
     toast("Delete post success 😈")  
     window.location.reload()
-    // Optionally, update state to reflect deletion without a page reload
   };
   
   const handleInputChange = (e, rowId, field) => {
@@ -182,7 +166,7 @@ export default function App() {
   };
   const handleSave = async (row) => {
     const { data, error } = await supabase
-      .from('powerbi') // Replace with your table name
+      .from('powerbi')
       .update({
         title: row.title,
         content: row.content,
@@ -218,8 +202,6 @@ export default function App() {
                     >
                       <div className="p-4">
                         <h1 className="text-xl font-bold text-white">FES MANAGEMENT</h1>
-                        
-                        {/* <h1 className="text-m">By dunv</h1> */}
                       </div>
                       <nav className="flex-1 p-4">
                         {pages.map((page) => (
@@ -233,7 +215,7 @@ export default function App() {
                           >
                             {page === "Dashboard" && <Home className="mr-2 h-4 w-4 " />}
                             {page === "Database management" && <LayoutDashboard className="mr-2 h-4 w-4" />}
-                            {page === "User management" && <UserCog className="mr-2 h-4 w-4" />}
+                            {page === "Tutorial" && <GraduationCap className="mr-2 h-4 w-4" />}
                             {page}
                           </Button>
                         ))}
@@ -265,10 +247,6 @@ export default function App() {
 
                               <CardHeader>
                                 <CardTitle className="mb-5">Reports</CardTitle>
-                                <Marquee>
-                                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet purus nec dolor fringilla fermentum. Fusce dapibus sapien at est tincidunt, vitae feugiat magna viverra. Integer pharetra justo vel nibh facilisis, a scelerisque arcu gravida. Nam sodales, justo nec tincidunt consequat, sem eros fermentum ex, in porttitor turpis nulla in erat. Proin varius, justo sed aliquam sodales, felis velit condimentum ligula, vitae tincidunt justo purus nec arcu. Suspendisse potenti. Aliquam erat volutpat. Etiam euismod diam sit amet justo ultrices, in tincidunt lectus facilisis. Donec gravida cursus neque, id finibus purus porttitor in. Ut ac libero ac nunc tempus fermentum.
-                                  Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vivamus aliquet mi eget justo eleifend, non elementum ex tincidunt. Curabitur congue ligula a est lacinia, non lobortis metus gravida. Ut mollis ligula a diam tristique, non bibendum arcu tempor. Duis nec tincidunt ligula. Sed congue ultricies mi, id facilisis elit. Nulla facilisi. Aenean fringilla sem vel erat efficitur, sit amet vulputate tortor interdum. In in ipsum sit amet magna vestibulum ullamcorper.
-                                </Marquee> 
                               </CardHeader>
                               <CardContent>
                                   {/* <div className="mb-5 w-full flex flex-wrap md:flex-nowrap gap-5">
@@ -287,19 +265,27 @@ export default function App() {
                                         <img
                                           src={article.image}
                                           alt="somthing went wrong when server try to load this image. Please double check the link image"
-                                          className="object-cover h-48 w-full rounded-[10px]"
+                                          className="object-cover h-100 w-full rounded-[10px]"
                                         /> :
                                         <div className='flex flex-col space-y-3'>
                                           <Skeleton className="h-[125px] w-full rounded-xl" />
                                         </div>
                                       }
-                                        <p className="mt-4 text-muted-foreground line-clamp-3">{article.content}</p>
+                                        {/* <p className="mt-4 text-muted-foreground line-clamp-3">{article.content}</p> */}
+                                        <div className="mt-4 text-muted-foreground">
+                                          {article.content.split("•").map((item, index) => (
+                                            <p key={index} style={{ marginBottom: "8px" }}>
+                                              {index > 0 && "•"} {item.trim()}
+                                            </p>
+                                          ))}
+                                        </div>
+
                                       </CardContent>
                                       <CardFooter className="flex justify-between text-sm text-muted-foreground">
                                         
                                         <span>{article.modify}</span>
                                         {/* <Button onClick={() => openDrawer(article)} className='bg-[#2e2d32] rounded-lg'>View</Button> */}
-                                        <div className='pt-1 pr-5 pb-1 pl-5 rounded-[5px] text-white border-dashed border-2 border-black text-black' onClick={() => openDrawer(article)}>
+                                        <div className='pt-1 pr-5 pb-1 pl-5 rounded-[5px] bg-black text-white' onClick={() => openDrawer(article)}>
                                           View
                                         </div>
                                       </CardFooter>
@@ -330,7 +316,7 @@ export default function App() {
                                     </SheetHeader>
                                     <form onSubmit={ async (e)=> {
                                       e.preventDefault()
-                                      // Newrows()
+                                      
                                       const form = e.target as HTMLFormElement;
                                       const formData = new FormData(form);
                                       const new_title_row = formData.get('title')
@@ -361,6 +347,10 @@ export default function App() {
                                       <div>
                                         <Label htmlFor="title">Title, required</Label>
                                         <Input id="title" name="title" required />
+                                      </div>
+                                      <div>
+                                        <Label htmlFor="content">Content, not required</Label>
+                                        <Textarea id="content" name="content"/>
                                       </div>
                                       <div>
                                         <Label htmlFor="content">Content, not required</Label>
@@ -411,6 +401,7 @@ export default function App() {
                                           defaultValue={row.content}
                                           onChange={(e) => handleInputChange(e, row.id, 'content')}
                                           className="rounded-[12px]"
+                                          style={{ width: "100%", height: "200px", whiteSpace: "pre-wrap" }}
                                         />
                                       </TableCell>
                                       <TableCell>
@@ -473,6 +464,17 @@ export default function App() {
                             </Card>
                           
                           )}
+                          {activePage === "Tutorial" && (
+                            <Card className="w-full bg-[#1f1f1f] border-transparent text-white">
+                              <CardHeader>
+                                <CardTitle>Using the Power BI Dashboard for Industry Analysis in Sydney</CardTitle>
+                              </CardHeader>
+                              <CardContent>
+                                <Tutor></Tutor>
+
+                              </CardContent>
+                            </Card>
+                          )}
                           
                         </div>
                       </main>
@@ -505,16 +507,16 @@ export default function App() {
                             alt="somthing went wrong when server try to load this image. Please double check the link image"
                             className="object-cover"
                           /> */}
-                            <p className="text-lg text-muted-foreground">{selectedArticle.content}</p>
+                            {/* <p className="text-lg text-muted-foreground">{selectedArticle.content}</p> */}
+                            <div className="mt-4 text-muted-foreground">
+                              {selectedArticle.inside.split("•").map((item, index) => (
+                                <p key={index} style={{ marginBottom: "8px" }}>
+                                  {index > 0 && "•"} {item.trim()}
+                                </p>
+                              ))}
+                            </div>
+
                             <div className="aspect-video ">
-                              {/* <iframe
-                                title="Power BI Report"
-                                width="100%"
-                                height="100%"
-                                src={selectedArticle.powerBiLink}
-                                frameBorder="0"
-                                allowFullScreen
-                              ></iframe> */}
                               <iframe title="INFT 6900" width="100%" height="100%" src={selectedArticle.powerBiLink} frameborder="0" allowFullScreen="true" className='rounded-[12px] border-dashed border-2'></iframe>
                             </div>
                           </div>
@@ -530,7 +532,7 @@ export default function App() {
                 <div className='flex justify-center'>
                 
                   <SignedOut>
-                    {/* Hiển thị nếu người dùng chưa đăng nhập */}
+
                 <SignIn/>
                   </SignedOut>
                 </div>
